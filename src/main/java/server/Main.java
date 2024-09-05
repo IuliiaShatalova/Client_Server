61b06f5652460cdf18eb5a517965d513f1c5f0b7
JS <Yla45@yandex.ru>
2024-09-05 23:57:12 +0300

    Created ServerWindow class

---
+++ src/main/java/server/Main.java
@@ -0,0 +1,9 @@
+package server;
+
+import server.server.ServerWindow;
+
+public class Main {
+    public static void main(String[] args) {
+        ServerWindow serverWindow = new ServerWindow();
+    }
+}
